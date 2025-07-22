b21d15077f98a04f3b76c03fe305cf1220f841c0
riya01412 <riyadesai0140@gmail.com>
2025-07-22 11:25:15 +0530

    Create math_utils.py

---
+++ src/math_utils.py
@@ -0,0 +1,10 @@
+def add(a,b):
+  """
+  add two numbers and return the result.
+  parameters:
+  a (int): first number
+  b (int): second number
+  returns:
+  int:sum of a and b
+  """
+  return a+b
